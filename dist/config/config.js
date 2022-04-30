57bff132db0f1ad4c158b2081904d4a8493ea27b
mahan ms <genius20120ms@gmail.com>
2022-04-30 07:22:24 +0430

    delete dist

--- dist/config/config.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.default = {
-    JWT_SECRET: "secret",
-    BCRYPT_SALT: 8,
-    JWT_EXPIRED_TIME: "3600s",
-};
